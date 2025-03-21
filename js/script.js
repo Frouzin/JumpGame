49af19256cd8a7dd310f7c5f4a29ad32cd4b15b9
Matheus Reis <105878769+Frouzin@users.noreply.github.com>
2025-03-21 03:02:56 +0000

    add mapgame and animation endgame

--- js/script.js
+++ js/script.js
@@ -9,11 +9,22 @@ const jump = () => {
     },500);
 }
 const loop = setInterval(()=> {
+
     const nessPosition = +window.getComputedStyle(ness).bottom.replace('px', '');
     const pipePosition = pipe.offsetLeft;
+
     if(pipePosition <= 100 && pipePosition > 0 && nessPosition <80 ){
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        ness.style.animation = 'none';
+        ness.style.bottom = `${nessPosition}px`;
+
+        ness.src = './images/ness-earthbound.gif';
+        ness.style.width = '90px';
+        ness.style.marginLeft = '10px';
+
+        clearInterval(loop);
 
     }
 },10);
