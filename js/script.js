94dfd32af7016f1bac28437a188582be1ea28ba4
Matheus Reis <105878769+Frouzin@users.noreply.github.com>
2025-03-21 02:48:54 +0000

    add: colision the character

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const ness = document.querySelector('.ness');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     ness.classList.add('jump');
@@ -7,5 +8,14 @@ const jump = () => {
     ness.classList.remove('jump');
     },500);
 }
+const loop = setInterval(()=> {
+    const nessPosition = +window.getComputedStyle(ness).bottom.replace('px', '');
+    const pipePosition = pipe.offsetLeft;
+    if(pipePosition <= 100){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+    }
+},10);
 
 document.addEventListener('keydown', jump);
